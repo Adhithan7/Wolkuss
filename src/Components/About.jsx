cabe1a75fd26763df4cc53a009a076c697b8e5cb
Adhithan C <82218804+Adhithan7@users.noreply.github.com>
2024-06-04 17:53:56 +0530

    Add files via upload

---
+++ src/Components/About.jsx
@@ -0,0 +1,19 @@
+function About() {
+  return (
+    <div className="bg-[#000000] h-[48rem] flex flex-col items-center justify-center text-[white] font-semibold lg:h-[52rem] lg:flex lg:w-[100%]">
+      <h1 className="text-center text-[1.7rem]">About Us</h1>
+      <p className="text-center text-[1.3rem]">"We are Movieflix"</p>
+      <p className="text-center w-[20rem] text-[1.2rem] md:w-[23rem] lg:w-[30rem] xl:w-[35rem]">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
+      <br />
+      <h1 className="text-center text-[1.7rem]">Address Details</h1>
+      <p className="text-center w-[20rem] text-[1.2rem] lg:w-[30rem]">
+        Chennai, India, Tele ph:+135930045855, City: Port Lazanya, Zip
+        code:7867374-772, E-mail:jhajiahat@johnson.com
+      </p>
+    </div>
+  );
+}
+
+export default About;
